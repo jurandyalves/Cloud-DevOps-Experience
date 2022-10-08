c430e78f77f176dd985fa528afd8ae34c94d11e5
jurandyalves <jurandyalves@hotmail.com>
2022-10-08 16:28:40 -0300

    concluído

--- Script de Criação de Estrutura de Usuários, Diretórios e Permissões/desafio01.sh
+++ Script de Criação de Estrutura de Usuários, Diretórios e Permissões/desafio01.sh
@@ -13,13 +13,25 @@ groupadd GRP_SEC
 
 echo "Criando usuários, seus diretórios e vinculando aos seus grupos..."
 useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-useradd debora
-useradd josefina
-useradd maria
-useradd sebastiana
-useradd amanda
-useradd joao_
-useradd roberto
-useradd rogerio
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+
+echo "Especificando permissões de diratórios..."
+chown root:GRP_ADM /adm
+chown root:GRP_SEC /sec
+chown root:GRP_VEN /ven
+
+chmod 770 /adm
+chmod 770 /sec
+chmod 770 /ven
+chmod 777 /publico
 
 echo "Processo finalizado!"
